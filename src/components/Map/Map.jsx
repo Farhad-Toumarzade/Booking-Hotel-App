ab1e65dd7099d5adfe0c0785ca78c78843f81e45
Farhad-Toumarzade <farhad.thw2@gmail.com>
2023-11-06 18:08:34 +0330

    show hotels on map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,30 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useHotels } from "../HotelProvider/HotelsProvider";
+import { useState } from "react";
 
 function Map() {
-   const{isLoading, hotels} useHotels()
-  return <div className="mapCountainer">map</div>;
+  const { isLoading, hotels } = useHotels();
+  const [mapCenter, setMapCenter] = useState([50, 3]);
+  return (
+    <div className="mapContainer">
+      <MapContainer
+        className="map"
+        center={mapCenter}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {hotels.map((item) => (
+          <Marker key={item.id} position={[item.latitude, item.longitude]}>
+            <Popup>{item.host_location}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
 }
 
 export default Map;
